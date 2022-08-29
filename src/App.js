28fed58a5d11bf3d83611d74da951839716ccce8
Rahim-sarker <mhrahim08@gmail.com>
2022-08-29 13:26:43 +0600

    Calender is done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Login from "./pages/Login/Login";
+import Appointment from "./pages/Appointment/Appointment";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="appoinment" element={<Appointment />} />
         <Route path="login" element={<Login />} />
       </Routes>
     </div>
